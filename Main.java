e84e8d0cf797cb0ae966881e53e9bcef07e4e371
AndrewLevada <levada.andrew@gmail.com>
2023-09-26 20:20:52 +0300

    :sparkles: added example of basic classes usage into Main

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
+import structures.Matrix;
+import structures.MatrixFactory;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
+
+        // Example
+
+        Matrix A = MatrixFactory.createMatrixFromInput();
+        System.out.println(A.getInverse());
     }
 }
