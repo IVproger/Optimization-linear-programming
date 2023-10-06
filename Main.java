47565019821cb99845be06ef0f21f0bcd42422f8
Native-Kira-Karina <kirastr354@gmail.com>
2023-10-06 17:07:57 +0300

    approximation accuracy is don

--- Main.java
+++ Main.java
@@ -30,7 +30,7 @@ public class Main {
         System.out.println("Enter a vector of right-hand side numbers - b.");
         Vector b = VectorFactory.createVectorFromInput(m, scanner);
         System.out.println("Enter an approximation accuracy - epsilon.");
-        double e = scanner.nextDouble();
+        String e = scanner.next();
 
         Simplex simplex = Simplex.builder()
                 .setOptimize(opt)
@@ -53,7 +53,7 @@ class Simplex {
     Vector c; // (N)
     Matrix A; // (M*N)
     Vector b; // (M)
-    double epsilon; // approximation accuracy
+    String epsilon; // approximation accuracy
     Vector basisIndexes;
     Vector cBasis;
     Vector xBasis;
@@ -69,7 +69,7 @@ class Simplex {
         optimize = "MAX";
         numberOfVariablesN = 0;
         numberOfConstraintsM = 0;
-        epsilon = 0;
+        epsilon = "";
     }
 
     int checkApplicability() {
@@ -151,10 +151,20 @@ class Simplex {
             xSolution.setItem((int) basisIndexes.getItem(i), xBasis.getItem(i));
         }
         System.out.print("A vector of decision variables - x* = {");
+        int signs = 0;
+        boolean afterComma = false;
+        for (int i = 0; i < epsilon.length(); i++) {
+            if (epsilon.charAt(i) == '.' || epsilon.charAt(i) == ',') {
+                afterComma = true;
+            }
+            if (afterComma && epsilon.charAt(i) >= '0' && epsilon.charAt(i) <= '9') {
+                signs++;
+            }
+        }
+        String approximation = "%." + signs + "f ";
         for (double x : xSolution
         ) {
-
-            System.out.print(x + " ");
+            System.out.printf(approximation, x);
         }
         System.out.println("}");
         if (optimize.equalsIgnoreCase("max")) {
@@ -163,7 +173,7 @@ class Simplex {
         if (optimize.equalsIgnoreCase("min")) {
             System.out.print("Minimum ");
         }
-        System.out.println("value of the objective function " + solution);
+        System.out.printf("value of the objective function " + approximation, solution);
     }
 
     void setNonBasicVariables() {
@@ -219,7 +229,7 @@ class Simplex {
         } else if (optimize.equalsIgnoreCase("min")) {
             enteringVector = maxPositiveItemIndex(zRow);
         }
-        if(enteringVector==-1){
+        if (enteringVector == -1) {
             return enteringVector;
         }
         return (int) nonBasicIndexes.getItem(enteringVector);
@@ -243,7 +253,7 @@ class Simplex {
                 res = i;
             }
         }
-        if(res==-1){
+        if (res == -1) {
             return res;
         }
         return (int) basisIndexes.getItem(res);
@@ -252,7 +262,7 @@ class Simplex {
     void changeBasis(int leaving, int entering) {
         int indLeaving = 0;
         for (int i = 0; i < basisIndexes.getLength(); i++) {
-            if(basisIndexes.getItem(i)==leaving){
+            if (basisIndexes.getItem(i) == leaving) {
                 indLeaving = i;
                 break;
             }
@@ -270,7 +280,7 @@ class Simplex {
         private Vector c; // (N)
         private Matrix A; // (M*N)
         private Vector b; // (M)
-        private double epsilon; // approximation accuracy
+        private String epsilon; // approximation accuracy
 
         Builder setOptimize(String optimize) {
             this.optimize = optimize;
@@ -302,7 +312,7 @@ class Simplex {
             return this;
         }
 
-        Builder setEpsilon(double e) {
+        Builder setEpsilon(String e) {
             this.epsilon = e;
             return this;
         }
